f37ba26f2147983c838a1afff76438fc27f2674a
h0nda <41300123+h0nde@users.noreply.github.com>
2021-09-18 12:30:19 +0200

    clean up args

--- lib/arguments.py
+++ lib/arguments.py
@@ -14,13 +14,55 @@ def parse_range(range_string):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-w", "--workers", type=int, default=cpu_count(), help="Number of workers")
-    parser.add_argument("-t", "--threads", type=int, default=50, help="Number of threads (per worker)")
-    parser.add_argument("-r", "--range", type=parse_range, required=True, nargs="+", help="Range(s) of group IDs")
-    parser.add_argument("-c", "--cut-off", type=parse_human_number, help="ID limit for skipping missing groups")
-    parser.add_argument("-p", "--proxy-file", type=argparse.FileType("r", encoding="UTF-8", errors="ignore"), help="File containing HTTP proxies")
-    parser.add_argument("-u", "--webhook-url", type=str, help="Send group results to <url>")
-    parser.add_argument("-C", "--chunk-size", type=int, default=100, help="Number of groups to be sent per batch request")
-    parser.add_argument("-T", "--timeout", type=float, default=5.0, help="Timeout for connections and responses")
+    parser.add_argument(
+        "-w", "--workers",
+        default=cpu_count(),
+        type=int,
+        required=True,
+        help="Number of workers",
+        metavar="<num>")
+    parser.add_argument(
+        "-t", "--threads",
+        default=50,
+        type=int,
+        required=True,
+        help="Number of threads (per worker)",
+        metavar="<num>")
+    parser.add_argument(
+        "-r", "--range",
+        nargs="+",
+        type=parse_range,
+        required=True,
+        help="Range(s) of group IDs",
+        metavar="<range>")
+    parser.add_argument(
+        "-p", "--proxy-file",
+        type=argparse.FileType("r", encoding="UTF-8", errors="ignore"),
+        help="File containing HTTP proxies", 
+        metavar="<file>")
+    parser.add_argument(
+        "-u", "--webhook-url",
+        type=str,
+        help="Send group results to <url>",
+        metavar="<url>")
+    parser.add_argument(
+        "-c", "--cut-off",
+        type=parse_human_number,
+        help="ID limit for skipping missing groups",
+        metavar="<id>")
+    parser.add_argument(
+        "-C", "--chunk-size",
+        default=100, 
+        type=int,
+        required=True,
+        help="Number of groups to be sent per batch request",
+        metavar="<size>")
+    parser.add_argument(
+        "-T", "--timeout",
+        default=5.0,
+        type=float,
+        required=True,
+        help="Timeout for connections and responses",
+        metavar="<seconds>")
     arguments = parser.parse_args()
     return arguments
