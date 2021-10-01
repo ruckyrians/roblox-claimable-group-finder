bb01fd6c324120a01625fa50b69212e1bd80c3e4
h0nda <41300123+h0nde@users.noreply.github.com>
2021-10-01 19:55:03 +0200

    Update threads.py

--- lib/threads.py
+++ lib/threads.py
@@ -71,7 +71,7 @@ def group_scanner(log_queue, count_queue, proxy_iter, timeout, webhook_url,
                 GROUP_API_ADDR,
                 timeout,
                 proxy_addr,
-                proxy_headers={"Authorization": proxy_auth} if proxy_auth else {},
+                proxy_headers={"Proxy-Authorization": proxy_auth} if proxy_auth else {},
                 hostname=GROUP_API)
         except:
             continue
